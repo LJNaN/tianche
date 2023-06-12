9199649b1d7b253d1c53b253583553eac6c17232
小明 <1135331731@qq.com>
2023-06-12 14:58:50 +0800

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,20 +1,30 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import * as echarts from "echarts";
+
+// let url = ref('./assets/3d/img/4.png')
 </script>
 
 <template>
-  <div class="header">headerheaderheaderheaderheaderheader</div>
+  <div class="header">
+    <!-- <img :src="url"> -->
+  </div>
+  
 </template>
 
 <style scoped>
 .header {
+  background: url('/assets/3d/img/4.png') center / 100% 100% no-repeat;
   border: 1px solid red;
   position: absolute;
   left: 0;
   top: 0;
   width: 100%;
-  height: 30%;
+  height: 10%;
   z-index: 2;
+  pointer-events: all;
+}
+.img {
+
 }
 </style>
--- src/views/home/right.vue
+++ src/views/home/right.vue
@@ -1,15 +1,158 @@
-<script setup>
-import { onMounted, ref } from "vue";
-import * as echarts from "echarts";
-</script>
-
 <template>
   <div class="right">
     homerighthomerighthomerighthomerighthomerighthomerighthomerighthomeright
+    <div class="echart1" ref="echart1"></div>
   </div>
 </template>
 
-<style scoped>
+<script setup>
+import { onMounted, ref } from "vue";
+import * as echarts from "echarts";
+let echart1 = ref(null);
+
+onMounted(() => {
+  console.log(echart1.value);
+  const myChart1 = echarts.init(echart1.value);
+  const myChart1Option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        // 坐标轴指示器，坐标轴触发有效
+        type: "shadow", // 默认为直线，可选为：'line' | 'shadow'
+      },
+      //formatter: '{a}<br />{c}'
+    },
+    backgroundColor: "#0c2d55",
+    color: ["#18f0fd", "#00c4fd", "#3f90ff"],
+    legend: {
+      show: true,
+      top: "2%",
+      icon: "rect",
+      itemWidth: 15, // 图例标记的图形宽度。
+      itemHeight: 15, //  图例标记的图形高度。
+      textStyle: {
+        color: "#fff",
+        fontSize: 14,
+        padding: [0, 8, 0, 8],
+      },
+    },
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        axisLine: {
+          //坐标轴轴线相关设置。数学上的x轴
+          show: true,
+          lineStyle: {
+            color: "rgb(41,188,245)",
+          },
+        },
+        axisLabel: {
+          //坐标轴刻度标签的相关设置
+          textStyle: {
+            color: "#FFFFFF",
+            fontSize: 12,
+          },
+        },
+        splitLine: {
+          show: false,
+          lineStyle: {
+            color: "#233653",
+          },
+        },
+        axisTick: {
+          show: false,
+        },
+        data: [
+          "2022年12月",
+          "2023年1月",
+          "2023年2月",
+          "2023年3月",
+          "2023年4月",
+          "2023年5月",
+        ],
+      },
+    ],
+    yAxis: [
+      {
+        name: "t",
+        nameTextStyle: {
+          color: "#fff",
+          fontSize: 12,
+          padding: [0, 60, 0, 0],
+        },
+        // minInterval: 1,
+        type: "value",
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "#1160a0",
+            type: "dashed",
+          },
+        },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "#008de7",
+          },
+        },
+        axisLabel: {
+          show: true,
+          textStyle: {
+            color: "#fff",
+            fontSize: 14,
+          },
+        },
+        axisTick: {
+          show: false,
+        },
+        splitLine: false,
+      },
+    ],
+    series: [
+      {
+        name: "1",
+        type: "bar",
+        stack: "搜索引擎",
+        barWidth: 40,
+        label: {
+          show: false,
+        },
+        data: [620, 732, 701, 734, 1090, 1130, 1120],
+      },
+      {
+        name: "2",
+        type: "bar",
+        label: {
+          show: false,
+        },
+        stack: "搜索引擎",
+        data: [120, 132, 101, 134, 290, 230, 220],
+      },
+      {
+        name: "3",
+        type: "bar",
+
+        label: {
+          show: false,
+        },
+        stack: "搜索引擎",
+        data: [120, 132, 101, 134, 290, 230, 220],
+      },
+    ],
+  };
+  myChart1.setOption(myChart1Option);
+  window.addEventListener('resize', myChart1.resize)
+});
+</script>
+
+
+<style lang="less" scoped>
 .right {
   border: 1px solid red;
   word-break: break-all;
@@ -19,5 +162,11 @@ import * as echarts from "echarts";
   width: 30%;
   height: 100%;
   z-index: 2;
+
+  .echart1 {
+    pointer-events: all;
+    height: 50%;
+    width: 100%;
+  }
 }
 </style>
