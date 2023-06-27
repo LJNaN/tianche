5e5089f2ccf2069fe14b5e37bf30637de3e5508d
小明 <1135331731@qq.com>
2023-06-27 10:06:55 +0800

    提交

--- src/components/header.vue
+++ src/components/header.vue
@@ -26,6 +26,9 @@ const options = reactive(["天车", "轨道", "OHB", "卡匣", "设备", "指令
 
 <template>
   <div class="header">
+    <div class="yyyy">
+
+    </div>
     <div class="xlk">
       <Dropdown :options="['天车', '轨道', 'OHB', '卡匣', '设备', '指令']" />
 
@@ -64,6 +67,12 @@ const options = reactive(["天车", "轨道", "OHB", "卡匣", "设备", "指令
 </template>
 
 <style scoped lang='less'>
+.yyyy{
+  border: 1px solid red;
+  position: absolute;
+  width: 10%;
+  height: 10%;
+}
 .my-p {
   // border: 1px solid red;
   word-break: break-all;
