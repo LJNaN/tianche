1dd1d6371f979673d543905492f33de3adf53ab1
小明 <1135331731@qq.com>
2023-08-07 17:54:18 +0800

    没问题了

--- src/views/home/yc.vue
+++ src/views/home/yc.vue
@@ -2,7 +2,7 @@
  * @Author: 小明 1135331731@qq.com
  * @Date: 2023-08-07 16:45:34
  * @LastEditors: 小明 1135331731@qq.com
- * @LastEditTime: 2023-08-07 16:57:27
+ * @LastEditTime: 2023-08-07 17:52:47
  * @FilePath: \project-tianche\src\views\home\yc.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -29,7 +29,7 @@ function isshow() {
 
 .right{
   pointer-events: all;
-  background: url("/assets/3d/img/49.png") center / 100% 100% no-repeat;
+  background: url("/assets/3d/img/49.png") center / 90% 100% no-repeat;
   // border: 1px solid red;
   position: absolute;
   width: 2%;
