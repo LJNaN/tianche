ac0552f9cbbf0ebab0810a84a36b5767097cd960
江南 <495587206@qq.com>
2023-08-22 21:08:04 +0800

    完成指南针组件

--- src/components/compass.vue
+++ src/components/compass.vue
@@ -1,11 +1,38 @@
 <script setup>
 import { CACHE } from '@/ktJS/CACHE'
+import { STATE } from '@/ktJS/STATE'
 import { ref, render } from 'vue'
+import * as TWEEN from '@tweenjs/tween.js'
+
 
 let deg = ref(0)
-setTimeout(() => {
-  init()
-}, 3000)
+
+const imgList = [[4, 10], [2, 7], [1, 8], [3, 9]]
+
+let tweenRenderFlag = ref(false)
+
+let imgSrc = ref([
+  `/assets/3d/img/compass/${imgList[0][0]}.png`,
+  `/assets/3d/img/compass/${imgList[1][0]}.png`,
+  `/assets/3d/img/compass/${imgList[2][0]}.png`,
+  `/assets/3d/img/compass/${imgList[3][0]}.png`
+])
+
+let rotateDownFlag = ref(false)
+let zoomDownFlag = ref(false)
+
+wait()
+function wait() {
+  if (CACHE?.container?.orbitControls) {
+    init()
+  } else {
+    setTimeout(() => {
+      wait()
+    }, 500)
+  }
+}
+
+
 function init() {
   const control = CACHE.container.orbitControls
   control.addEventListener('change', () => {
@@ -22,16 +49,6 @@ function init() {
   })
 }
 
-const imgList = [[4, 10], [2, 7], [1, 8], [3, 9]]
-
-let imgSrc = ref([
-  `/assets/3d/img/compass/${imgList[0][0]}.png`,
-  `/assets/3d/img/compass/${imgList[1][0]}.png`,
-  `/assets/3d/img/compass/${imgList[2][0]}.png`,
-  `/assets/3d/img/compass/${imgList[3][0]}.png`
-])
-
-let rotateUpFlag = ref(false)
 
 function hoverDial(id) {
   imgSrc.value[id] = `/assets/3d/img/compass/${imgList[id][1]}.png`
@@ -42,21 +59,20 @@ function leaveDial(id) {
 function downDial(id) {
   const control = CACHE.container.orbitControls
 
-
   if (id === 1) {
-    control.autoRotateSpeed = 3
-    control.autoRotate = true
-
-  } else if (id === 3) {
     control.autoRotateSpeed = -3
     control.autoRotate = true
 
+  } else if (id === 3) {
+    control.autoRotateSpeed = 3
+    control.autoRotate = true
+
   } else if (id === 0 || id === 2) {
-    rotateUpFlag.value = true
+    rotateDownFlag.value = true
     render()
     function render() {
-      if (rotateUpFlag.value) {
-        control.rotateUp(id === 0 ? 0.01 : -0.01)
+      if (rotateDownFlag.value) {
+        control.rotateUp(id === 0 ? 0.005 : -0.005)
         control.update()
         requestAnimationFrame(render)
       }
@@ -70,98 +86,208 @@ function upDial(id) {
   if (id === 1 || id === 3) {
     control.autoRotate = false
   } else if (id === 0 || id === 2) {
-    rotateUpFlag.value = false
+    rotateDownFlag.value = false
   }
+}
+
+
+function clickArrow() {
+  tweenRenderFlag.value = true
+  const camera = CACHE.container.orbitCamera
+  const control = CACHE.container.orbitControls
+
+  TweenRender()
+  new TWEEN.Tween(camera.position)
+    .to({
+      x: STATE.initialState.position.x,
+      y: STATE.initialState.position.y,
+      z: STATE.initialState.position.z
+    }, 800)
+    .easing(TWEEN.Easing.Quadratic.InOut)
+    .start()
+
+  new TWEEN.Tween(control.target)
+    .to({
+      x: STATE.initialState.target.x,
+      y: STATE.initialState.target.y,
+      z: STATE.initialState.target.z
+    }, 800)
+    .easing(TWEEN.Easing.Quadratic.InOut)
+    .start()
+    .onComplete(() => {
+      tweenRenderFlag.value = false
+    })
+}
+
+function TweenRender() {
+  if (tweenRenderFlag.value) {
+    TWEEN.update()
+    requestAnimationFrame(TweenRender)
+  }
+}
+
+function zoomDown(type) {
+  
+  const control = CACHE.container.orbitControls
+  zoomDownFlag.value = true
+
+  render()
+  function render() {
+    
+    if (zoomDownFlag.value) {
+      control.dollyIn(type === 0 ? 0.99 : 1.01)
+      control.update()
+      requestAnimationFrame(render)
+    }
+  }
+}
+
+function zoomUp() {
+  zoomDownFlag.value = false
 }
 
 </script>
 
 <template>
   <div class="main">
-    <div class="arrow" :style="{ transform: `rotateZ(${deg}deg)` }"></div>
-    <div class="dial">
-      <img :src="imgSrc[0]" @mousedown="downDial(0)" @mouseup="upDial(0)" @mouseenter="hoverDial(0)"
-        @mouseleave="leaveDial(0)">
-      <img :src="imgSrc[1]" @mousedown="downDial(1)" @mouseup="upDial(1)" @mouseenter="hoverDial(1)"
-        @mouseleave="leaveDial(1)">
-      <img :src="imgSrc[2]" @mousedown="downDial(2)" @mouseup="upDial(2)" @mouseenter="hoverDial(2)"
-        @mouseleave="leaveDial(2)">
-      <img :src="imgSrc[3]" @mousedown="downDial(3)" @mouseup="upDial(3)" @mouseenter="hoverDial(3)"
-        @mouseleave="leaveDial(3)">
+    <div class="compass">
+      <div class="arrow" :style="{ transform: `rotateZ(${deg}deg)` }" @click="clickArrow()"></div>
+      <div class="dial">
+        <img :src="imgSrc[0]" @mousedown="downDial(0)" @mouseup="upDial(0)" @mouseenter="hoverDial(0)"
+          @mouseleave="leaveDial(0), upDial(0)">
+        <img :src="imgSrc[1]" @mousedown="downDial(1)" @mouseup="upDial(1)" @mouseenter="hoverDial(1)"
+          @mouseleave="leaveDial(1), upDial(1)">
+        <img :src="imgSrc[2]" @mousedown="downDial(2)" @mouseup="upDial(2)" @mouseenter="hoverDial(2)"
+          @mouseleave="leaveDial(2), upDial(2)">
+        <img :src="imgSrc[3]" @mousedown="downDial(3)" @mouseup="upDial(3)" @mouseenter="hoverDial(3)"
+          @mouseleave="leaveDial(3), upDial(3)">
+      </div>
+      <div class="compass-line"></div>
     </div>
-    <div class="line"></div>
+
+    <div class="zoom">
+      <div class="zoom-jia" @mousedown="zoomDown(0)" @mouseup="zoomUp()" @mouseleave="zoomUp()">+</div>
+      <div class="zoom-line"></div>
+      <div class="zoom-jian" @mousedown="zoomDown(1)" @mouseup="zoomUp()" @mouseleave="zoomUp()">-</div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="less">
 .main {
   position: absolute;
-  top: 10%;
-  left: 10%;
-  height: 100px;
   width: 100px;
-  border: 1px solid red;
-  pointer-events: all;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  div {
-    cursor: pointer;
-    user-select: none;
-    -webkit-user-drag: none;
-  }
-
-  .arrow {
+  .compass {
     position: absolute;
-    height: 100%;
+    height: 50%;
     width: 100%;
-    background: url('/assets/3d/img/compass/6.png') center / 100% 100% no-repeat;
-    z-index: 1;
-  }
+    pointer-events: all;
 
-  .dial {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-
-    img {
-      position: absolute;
-      width: 65%;
-      pointer-events: all;
-      z-index: 3;
+    div {
+      cursor: pointer;
       user-select: none;
       -webkit-user-drag: none;
     }
 
-    img:nth-of-type(1) {
-      left: 18%;
-      top: 4.5%;
+    .arrow {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background: url('/assets/3d/img/compass/6.png') center / 100% 100% no-repeat;
+      z-index: 3;
     }
 
-    img:nth-of-type(2) {
-      height: 65%;
-      width: auto;
-      right: 4.6%;
-      top: 18%;
+    .dial {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+
+      img {
+        position: absolute;
+        width: 65%;
+        pointer-events: all;
+        z-index: 3;
+        user-select: none;
+        -webkit-user-drag: none;
+      }
+
+      img:nth-of-type(1) {
+        left: 18%;
+        top: 5.7%;
+      }
+
+      img:nth-of-type(2) {
+        height: 65%;
+        width: auto;
+        right: 4.6%;
+        top: 18%;
+      }
+
+      img:nth-of-type(3) {
+        left: 18%;
+        bottom: 4%;
+      }
+
+      img:nth-of-type(4) {
+        height: 65%;
+        width: auto;
+        left: 5%;
+        top: 18%;
+      }
     }
 
-    img:nth-of-type(3) {
-      left: 18%;
-      bottom: 4%;
-    }
-
-    img:nth-of-type(4) {
-      height: 65%;
-      width: auto;
-      left: 4.6%;
-      top: 18%;
+    &-line {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      z-index: 2;
+      background: url('/assets/3d/img/compass/5.png') center / 95% 95% no-repeat;
     }
   }
 
-  .line {
+  .zoom {
     position: absolute;
-    height: 100%;
-    width: 100%;
-    z-index: 2;
-    background: url('/assets/3d/img/compass/5.png') center / 95% 95% no-repeat;
+    bottom: 0;
+    background-color: #fff;
+    border-radius: 6px;
+    width: 40%;
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &-jia,
+    &-jian {
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
+      pointer-events: all;
+      font-size: 2rem;
+      font-weight: bold;
+      line-height: 100%;
+      text-align: center;
+      color: #687386;
+    }
+
+    &-jia:hover,
+    &-jian:hover {
+      border-radius: 6px;
+      background-color: #e4e9f0;
+    }
+
+    &-line {
+      position: absolute;
+      top: 50%;
+      width: 100%;
+      height: 2px;
+      background-color: #e4e9f0;
+    }
   }
 }
 </style>
